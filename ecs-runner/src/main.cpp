f16fe513cb9dd277739181c5460ae11e2c691e1d
atironi <alessandro.tironi7@gmail.com>
2025-03-23 15:27:53 +0100

    Implemented simple test program.

--- ecs-runner/src/main.cpp
+++ ecs-runner/src/main.cpp
@@ -4,6 +4,7 @@
 #include <chrono>
 
 #include "World.h"
+#include "ArchetypeQuery.h"
 
 #define SDL_MAIN_HANDLED
 #include "SDL.h"
@@ -11,21 +12,81 @@
 
 bool s_keepUpdating = true;
 
-void StopAfter10Seconds()
+namespace comps 
 {
-    using namespace std::chrono_literals;
-    std::this_thread::sleep_for(5000ms);
+    struct Rect : public ecs::IComponent 
+    {
+        SDL_Rect rect;
+    };
 
-    s_keepUpdating = false;
+    struct Color : public ecs::IComponent 
+    {
+        SDL_Color color;
+    };
+
+    struct Velocity : public ecs::IComponent 
+    {
+        ecs::real_t x = 0.0f;
+        ecs::real_t y = 0.0f;
+    };
 }
+
+namespace systems 
+{
+    class MovementSystem : public ecs::ISystem 
+    {
+        void Update(std::weak_ptr<ecs::World> world, ecs::real_t deltaTime) override
+        {
+            ecs::query<comps::Rect, comps::Velocity>::MakeQuery(world).forEach(
+                [deltaTime](ecs::EntityHandle entity, comps::Rect& rect, comps::Velocity& velocity)
+                {
+                    rect.rect.x += static_cast<int>(velocity.x * deltaTime);
+                    rect.rect.y += static_cast<int>(velocity.y * deltaTime);
+
+                    // Simple bouncing off the edges of the screen
+                    if (rect.rect.x < 0 || rect.rect.x > 1024)
+                    {
+                        velocity.x *= -1.0f;
+                    }
+
+                    if (rect.rect.y < 0 || rect.rect.y > 720)
+                    {
+                        velocity.y *= -1.0f;
+                    }
+                });
+        }
+    };
+
+    class RenderSystem : public ecs::ISystem 
+    {
+    public:
+        RenderSystem() {}
+        RenderSystem(SDL_Renderer* renderer) : m_renderer(renderer) {}
+
+        void Update(std::weak_ptr<ecs::World> world, ecs::real_t deltaTime) override
+        {
+            ecs::query<comps::Rect, comps::Color>::MakeQuery(world).forEach(
+                [&](ecs::EntityHandle entity, comps::Rect& rect, comps::Color& color)
+                {
+                    SDL_SetRenderDrawColor(m_renderer, color.color.r, color.color.g, color.color.b, color.color.a);
+                    SDL_RenderFillRect(m_renderer, &rect.rect);
+                });
+        }
+
+        inline void SetRenderer(SDL_Renderer* renderer) { m_renderer = renderer; }
+
+    private:
+        SDL_Renderer* m_renderer;
+    };
+}
+
 
 int main()
 {
     using namespace std::chrono_literals;
 
     std::cout << "Initializing ECS instance..." << std::endl;
-    //ecs::Instance instance;
-    //instance.Initialize();
+    std::shared_ptr<ecs::World> world = std::make_shared<ecs::World>();
 
     // Initialize SDL
     std::cout << "Initializing SDL instance..." << std::endl;
@@ -37,7 +98,7 @@ int main()
     }
 
     // Create window
-    SDL_Window *window = SDL_CreateWindow("Basic C SDL project",
+    SDL_Window *window = SDL_CreateWindow("ECS Test Project",
                                           SDL_WINDOWPOS_UNDEFINED,
                                           SDL_WINDOWPOS_UNDEFINED,
                                           1024, 720,
@@ -61,6 +122,31 @@ int main()
 
     std::cout << "Running ECS instance..." << std::endl;
 
+    // Setup entities 
+    SDL_Rect rect(0, 0, 5, 5);
+    SDL_Color color(0, 128, 135, 255);
+    ecs::real_t maxVelocity = 200.0f;
+    size_t numEntities = 20;
+
+    // Create 20 entities with random positions, velocities, and colors
+    for (size_t i = 0; i < numEntities; ++i)
+    {
+        const ecs::entity_id entity = world->CreateEntity<comps::Velocity, comps::Rect, comps::Color>();
+        ecs::EntityHandle handle = world->GetEntity(entity);
+        rect.x = rand() % 1024; 
+        rect.y = rand() % 720;
+        handle.GetComponent<comps::Rect>().rect = rect;
+        handle.GetComponent<comps::Color>().color = color;
+        handle.GetComponent<comps::Velocity>().x = -1.0f + (2.0f * static_cast<float>(rand()) / RAND_MAX);
+        handle.GetComponent<comps::Velocity>().y = -1.0f + (2.0f * static_cast<float>(rand()) / RAND_MAX);
+        handle.GetComponent<comps::Velocity>().x *= maxVelocity;
+        handle.GetComponent<comps::Velocity>().y *= maxVelocity;
+    }
+
+    // Setup systems 
+    world->AddSystem<systems::MovementSystem>();
+    world->AddSystem<systems::RenderSystem>()->SetRenderer(renderer);
+
     auto previousTime = std::chrono::high_resolution_clock::now();
     auto currentTime = std::chrono::high_resolution_clock::now();
     while (s_keepUpdating)
@@ -68,8 +154,6 @@ int main()
         currentTime = std::chrono::high_resolution_clock::now();
         const auto elapsedTime = std::chrono::duration<float>(currentTime - previousTime);
         const float deltaTime = elapsedTime.count();
-        //instance.Update(deltaTime);
-
         previousTime = currentTime;
 
         SDL_Event e;
@@ -91,6 +175,8 @@ int main()
 
         // Set renderer color red to draw the square
         SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
+
+        world->Update(deltaTime);
 
         // Update screen
         SDL_RenderPresent(renderer);
