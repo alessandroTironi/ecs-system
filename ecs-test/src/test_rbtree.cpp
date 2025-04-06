03c1501a06b1f0e7856cf032c4afa27bbf5f365c
Alessandro Tironi <alessandro.tironi7@gmail.com>
2025-04-06 22:49:29 +0200

    Implemented tests for rbtree.erase() method.

--- ecs-test/src/test_rbtree.cpp
+++ ecs-test/src/test_rbtree.cpp
@@ -73,74 +73,6 @@ TEST_F(TestRBTree, TestInsertNonRootIdempotent)
     EXPECT_TRUE(tree.is_valid_tree());
 }
 
-TEST_F(TestRBTree, TestErase)
-{
-    ecs::rbtree<int> tree;
-    ASSERT_NO_THROW(tree.erase(1));
-    EXPECT_TRUE(tree.is_valid_tree());
-
-    tree.insert(1);
-    ASSERT_NO_THROW(tree.erase(1));
-    EXPECT_TRUE(tree.is_valid_tree());
-}
-
-TEST_F(TestRBTree, TestClear)
-{
-    ecs::rbtree<int> tree;
-    tree.insert(1);
-    tree.insert(2);
-    tree.insert(3);
-
-    ASSERT_NO_THROW(tree.clear());
-    EXPECT_EQ(tree.size(), 0);
-    EXPECT_TRUE(tree.is_valid_tree());
-}
-
-TEST_F(TestRBTree, TestFind)
-{
-    ecs::rbtree<int> tree;
-    tree.insert(1);
-    tree.insert(2);
-    tree.insert(3);
-
-    EXPECT_EQ(tree.find(4), tree.end());
-    EXPECT_NE(tree.find(1), tree.end());
-}
-
-TEST_F(TestRBTree, TestIterator)
-{
-    ecs::rbtree<int> tree;
-    tree.insert(1);
-    tree.insert(2);
-    tree.insert(3);
-
-    auto it = tree.begin();
-    EXPECT_EQ(it.value(), 1);
-    ++it;
-    EXPECT_EQ(it.value(), 2);
-    ++it;
-    EXPECT_EQ(it.value(), 3);
-    ++it;
-    EXPECT_EQ(it, tree.end());
-}
-
-TEST_F(TestRBTree, TestOrder)
-{
-    ecs::rbtree<int> tree;
-    tree.insert(5);
-    tree.insert(4);
-    tree.insert(3);
-    tree.insert(2);
-    tree.insert(1);
-
-    int prev = -1;
-    for (auto it = tree.begin(); it != tree.end(); ++it)
-    {
-        EXPECT_TRUE(it.value() > prev);
-        prev = it.value();
-    }
-}
-
 TEST_F(TestRBTree, TestSimpleRotations)
 {
     TestInsertionSequence({ 10, 5, 15, 3, 7, 12, 20 });
@@ -214,4 +146,247 @@ TEST_F(TestRBTree, TestPathologicalIncreasingSequence)
 TEST_F(TestRBTree, TestPathologicalDecreasingSequence)
 {
     TestInsertionSequence({ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 });
+}
+
+TEST_F(TestRBTree, TestErase)
+{
+    ecs::rbtree<int> tree;
+    ASSERT_NO_THROW(tree.erase(1));
+    EXPECT_TRUE(tree.is_valid_tree());
+
+    tree.insert(1);
+    ASSERT_NO_THROW(tree.erase(1));
+    EXPECT_TRUE(tree.is_valid_tree());
+}
+
+TEST_F(TestRBTree, TestEraseRedLeafNode)
+{
+    m_tree.insert(10);
+    m_tree.insert(5);
+    m_tree.insert(15);
+    m_tree.insert(3);
+
+    m_tree.erase(3);
+    EXPECT_TRUE(m_tree.is_valid_tree());
+}
+
+TEST_F(TestRBTree, TestEraseBlackNodeWithRedChild)
+{
+    m_tree.insert(20);
+    m_tree.insert(10);
+    m_tree.insert(30);
+    m_tree.insert(5);
+    m_tree.insert(15);
+
+    m_tree.erase(10);
+    EXPECT_TRUE(m_tree.is_valid_tree());
+}
+
+TEST_F(TestRBTree, TestEraseBlackLeafNode)
+{
+    m_tree.insert(20);
+    m_tree.insert(10);
+    m_tree.insert(30);
+    m_tree.insert(5);
+    m_tree.insert(25);
+    m_tree.insert(40);
+
+    m_tree.erase(5);
+    EXPECT_TRUE(m_tree.is_valid_tree());
+}
+
+TEST_F(TestRBTree, TestEraseNodeWithRedSibling)
+{
+    m_tree.insert(20);
+    m_tree.insert(10);
+    m_tree.insert(30);
+    m_tree.insert(5);
+    m_tree.insert(15);
+    m_tree.insert(40);
+    m_tree.insert(25);
+
+    m_tree.erase(5);
+    EXPECT_TRUE(m_tree.is_valid_tree());
+}
+
+TEST_F(TestRBTree, TestEraseWithBlackSiblingAndBlackNephews)
+{
+    m_tree.insert(20);
+    m_tree.insert(10);
+    m_tree.insert(30);
+    m_tree.insert(5);
+    m_tree.insert(15);
+    m_tree.insert(25);
+    m_tree.insert(40);
+    m_tree.insert(3);
+    m_tree.insert(7);
+
+    m_tree.erase(3);
+    EXPECT_TRUE(m_tree.is_valid_tree());
+}
+
+TEST_F(TestRBTree, TestEraseWithRedNearNephew)
+{
+    m_tree.insert(20);
+    m_tree.insert(10);
+    m_tree.insert(30);
+    m_tree.insert(5);
+    m_tree.insert(15);
+    m_tree.insert(25);
+    m_tree.insert(40);
+    m_tree.insert(17); 
+    
+    m_tree.erase(25);
+    EXPECT_TRUE(m_tree.is_valid_tree());
+}
+
+TEST_F(TestRBTree, TestEraseWithRedFarNephew)
+{
+    m_tree.insert(20);
+    m_tree.insert(10);
+    m_tree.insert(30);
+    m_tree.insert(5);
+    m_tree.insert(15);
+    m_tree.insert(25);
+    m_tree.insert(40);
+    m_tree.insert(13); 
+    
+    m_tree.erase(5);
+    EXPECT_TRUE(m_tree.is_valid_tree());
+}
+
+TEST_F(TestRBTree, TestEraseRootFromThreeNodeTree)
+{
+    m_tree.insert(20);
+    m_tree.insert(10);
+    m_tree.insert(30);
+    
+    m_tree.erase(20);
+    EXPECT_TRUE(m_tree.is_valid_tree());
+}
+
+TEST_F(TestRBTree, TestEraseRequiringMultipleRebalancing)
+{
+    TestInsertionSequence({50, 25, 75, 12, 37, 62, 87, 6, 18, 31, 43, 56, 68, 81, 93});
+
+    m_tree.erase(25);
+    EXPECT_TRUE(m_tree.is_valid_tree());
+
+    m_tree.erase(75);
+    EXPECT_TRUE(m_tree.is_valid_tree());
+
+    m_tree.erase(50);
+    EXPECT_TRUE(m_tree.is_valid_tree());
+}
+
+TEST_F(TestRBTree, SequentialDeletionsWithRebalancing)
+{
+    TestInsertionSequence({50, 25, 75, 12, 37, 62, 87, 6, 18, 31, 43, 56, 68, 81, 93});
+
+    std::vector<int> deleteOrder = {12, 87, 25, 56, 93, 37, 75, 6, 62, 43, 50, 31, 68, 18, 81};
+    for (int val: deleteOrder)
+    {
+        m_tree.erase(val);
+        ASSERT_TRUE(m_tree.is_valid_tree())
+            << "Lost balancing after removing " << val;
+    }
+
+    EXPECT_EQ(m_tree.size(), 0);
+}
+
+TEST_F(TestRBTree, TestEraseNodeWithBothChildren)
+{
+    m_tree.insert(50);
+    m_tree.insert(25);
+    m_tree.insert(75);
+    m_tree.insert(12);
+    m_tree.insert(37);
+    m_tree.insert(62);
+    m_tree.insert(87);
+    
+    // Delete 25, which has both 12 and 37 as children
+    m_tree.erase(25);
+    ASSERT_TRUE(m_tree.is_valid_tree());
+    
+    // Delete 75, which has both 62 and 87 as children
+    m_tree.erase(75);
+    ASSERT_TRUE(m_tree.is_valid_tree());
+}
+
+TEST_F(TestRBTree, TestEraseCausingFixupToRoot)
+{
+    m_tree.insert(10);
+    m_tree.insert(5);
+    m_tree.insert(20);
+    m_tree.insert(3);
+    m_tree.insert(7);
+    m_tree.insert(15);
+    m_tree.insert(25);
+    
+    // Delete in sequence to force cascading fixup
+    m_tree.erase(3);
+    ASSERT_TRUE(m_tree.is_valid_tree());
+    
+    m_tree.erase(7);
+    ASSERT_TRUE(m_tree.is_valid_tree());
+    
+    m_tree.erase(5); 
+    ASSERT_TRUE(m_tree.is_valid_tree());
+}
+
+TEST_F(TestRBTree, TestClear)
+{
+    ecs::rbtree<int> tree;
+    tree.insert(1);
+    tree.insert(2);
+    tree.insert(3);
+
+    ASSERT_NO_THROW(tree.clear());
+    EXPECT_EQ(tree.size(), 0);
+    EXPECT_TRUE(tree.is_valid_tree());
+}
+
+TEST_F(TestRBTree, TestFind)
+{
+    ecs::rbtree<int> tree;
+    tree.insert(1);
+    tree.insert(2);
+    tree.insert(3);
+
+    EXPECT_EQ(tree.find(4), tree.end());
+    EXPECT_NE(tree.find(1), tree.end());
+}
+
+TEST_F(TestRBTree, TestIterator)
+{
+    ecs::rbtree<int> tree;
+    tree.insert(1);
+    tree.insert(2);
+    tree.insert(3);
+
+    auto it = tree.begin();
+    EXPECT_EQ(it.value(), 1);
+    ++it;
+    EXPECT_EQ(it.value(), 2);
+    ++it;
+    EXPECT_EQ(it.value(), 3);
+    ++it;
+    EXPECT_EQ(it, tree.end());
+}
+
+TEST_F(TestRBTree, TestOrder)
+{
+    ecs::rbtree<int> tree;
+    tree.insert(5);
+    tree.insert(4);
+    tree.insert(3);
+    tree.insert(2);
+    tree.insert(1);
+
+    int prev = -1;
+    for (auto it = tree.begin(); it != tree.end(); ++it)
+    {
+        EXPECT_TRUE(it.value() > prev);
+        prev = it.value();
+    }
 }
