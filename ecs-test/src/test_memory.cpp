5f9201b588430cfd2ba177bb65f09d2aabe945f9
Alessandro Tironi <alessandro.tironi7@gmail.com>
2025-04-10 21:39:44 +0200

    Refactored test_memory to allow quick testing of any single block allocator.

--- ecs-test/src/test_memory.cpp
+++ ecs-test/src/test_memory.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include "Containers/memory.h"
 #include "Containers/SingleBlockFreeListAllocator.h"
+#include "Containers/SingleBlockChunkAllocator.h"
 
 using ::testing::Test; 
 
@@ -16,57 +17,87 @@ protected:
     {
 
     }
+
+    template<typename Allocator>
+    void TestSingleConstructorAndDestructor()
+    {
+        Allocator intAllocator;
+        EXPECT_EQ(intAllocator.usedCount(), 0);
+        EXPECT_EQ(intAllocator.freeCount(), 8);
+        EXPECT_EQ(intAllocator.capacity(), 8);
+    }
+
+    template<typename Allocator>
+    void TestSingleAllocation()
+    {
+        Allocator intAllocator;
+        size_t index;
+        ASSERT_NO_THROW(index = intAllocator.AllocateBlock());
+
+        EXPECT_EQ(intAllocator.usedCount(), 1);
+        EXPECT_EQ(intAllocator.freeCount(), 7);
+    }
+
+    template<typename Allocator>
+    void TestSingleDeallocation()
+    {
+        Allocator intAllocator;
+        const size_t index = intAllocator.AllocateBlock();
+
+        intAllocator.FreeBlock(index);
+        EXPECT_EQ(intAllocator.usedCount(), 0);
+        EXPECT_EQ(intAllocator.freeCount(), 8);
+    }
+
+    template<typename Allocator>
+    void TestAccess()
+    {
+        Allocator intAllocator;
+        const size_t index = intAllocator.AllocateBlock();
+        intAllocator[index] = 5;
+
+        EXPECT_EQ(intAllocator[index], 5);
+    }
+
+    template<typename Allocator>
+    void TestResize()
+    {
+        Allocator intAllocator;
+        intAllocator.AllocateBlock();
+        intAllocator.AllocateBlock();
+        intAllocator[0] = 10;
+        intAllocator[1] = 100;
+
+        intAllocator.AllocateBlock();
+        intAllocator[2] = 1000;
+        EXPECT_EQ(intAllocator.capacity(), 4);
+        EXPECT_EQ(intAllocator[0], 10);
+        EXPECT_EQ(intAllocator[1], 100);
+        EXPECT_EQ(intAllocator[2], 1000);
+    }
 };
 
 TEST_F(TestMemory, TestSingleBlockAllocatorConstructorAndDestructor)
 {
-    ecs::SingleBlockFreeListAllocator<int, 8> intAllocator;
-    EXPECT_EQ(intAllocator.usedCount(), 0);
-    EXPECT_EQ(intAllocator.freeCount(), 8);
-    EXPECT_EQ(intAllocator.capacity(), 8);
+    TestSingleConstructorAndDestructor<ecs::SingleBlockFreeListAllocator<int, 8>>();
 }
 
 TEST_F(TestMemory, TestSingleBlockAllocation)
 {
-    ecs::SingleBlockFreeListAllocator<int, 8> intAllocator;
-    size_t index;
-    ASSERT_NO_THROW(index = intAllocator.AllocateBlock());
-
-    EXPECT_EQ(intAllocator.usedCount(), 1);
-    EXPECT_EQ(intAllocator.freeCount(), 7);
+    TestSingleAllocation<ecs::SingleBlockFreeListAllocator<int, 8>>();
 }
 
 TEST_F(TestMemory, TestSingleBlockDeallocation)
 {
-    ecs::SingleBlockFreeListAllocator<int, 8> intAllocator;
-    const size_t index = intAllocator.AllocateBlock();
-
-    intAllocator.FreeBlock(index);
-    EXPECT_EQ(intAllocator.usedCount(), 0);
-    EXPECT_EQ(intAllocator.freeCount(), 8);
+    TestSingleDeallocation<ecs::SingleBlockFreeListAllocator<int, 8>>();
 }
 
 TEST_F(TestMemory, TestSingleBlockAccess)
 {
-    ecs::SingleBlockFreeListAllocator<int, 8> intAllocator;
-    const size_t index = intAllocator.AllocateBlock();
-    intAllocator[index] = 5;
-
-    EXPECT_EQ(intAllocator[index], 5);
+    TestAccess<ecs::SingleBlockFreeListAllocator<int, 8>>();
 }
 
 TEST_F(TestMemory, TestSingleBlockAllocatorResize)
 {
-    ecs::SingleBlockFreeListAllocator<int, 2> intAllocator;
-    intAllocator.AllocateBlock();
-    intAllocator.AllocateBlock();
-    intAllocator[0] = 10;
-    intAllocator[1] = 100;
-
-    intAllocator.AllocateBlock();
-    intAllocator[2] = 1000;
-    EXPECT_EQ(intAllocator.capacity(), 4);
-    EXPECT_EQ(intAllocator[0], 10);
-    EXPECT_EQ(intAllocator[1], 100);
-    EXPECT_EQ(intAllocator[2], 1000);
+    TestResize<ecs::SingleBlockFreeListAllocator<int, 2>>();
 }
