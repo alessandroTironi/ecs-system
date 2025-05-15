f597538faeb10a915110c37572ce5a99b7721f5d
Alessandro Tironi <alessandro.tironi7@gmail.com>
2025-05-15 22:18:23 +0200

    Implemented multicast delegates.

--- ecs-core/include/Core/Delegates.h
+++ ecs-core/include/Core/Delegates.h
@@ -1,7 +1,9 @@
 #pragma once 
 
 #include <functional>
-
+#include <vector>
+#include <algorithm>
+#include "Core/IDGenerator.h"
 
 namespace ecs 
 {
@@ -31,16 +33,65 @@ namespace ecs
 
         static delegate_t<TReturnType, TArgs...> create(FunctionType&& function)
         {
-            return delegate_t<TReturnType, TArgs...>(std::move(function));
+            return delegate_t<TReturnType, TArgs...>(std::move(function),
+                s_idGenerator.GenerateNewUniqueID());
+        }
+
+        bool operator==(const delegate_t<TReturnType, TArgs...>& other) const noexcept
+        {
+            return m_uniqueID == other.m_uniqueID;
+        }
+
+        inline size_t unique_id() const noexcept { return m_uniqueID; }
+
+    private:
+        delegate_t(FunctionType function, const size_t inID) 
+            : m_function{function}, m_uniqueID{inID} {}
+
+        FunctionType m_function = nullptr;
+        size_t m_uniqueID;
+
+        inline static IDGenerator<size_t> s_idGenerator;
+    };
+
+    template<typename... TArgs>
+    struct mc_delegate_t
+    {
+        using FunctionType = std::function<void(TArgs...)>;
+        using DelegateType = delegate_t<void, TArgs...>;
+
+        mc_delegate_t() = default;
+
+        void broadcast(TArgs... args)
+        {
+            for (auto& delegate : m_delegates)
+            {
+                delegate.invoke(args...);
+            }
+        }
+
+        void operator+=(DelegateType delegate)
+        {
+            m_delegates.push_back(delegate);
+        }
+
+        void operator-=(DelegateType delegate)
+        {
+            m_delegates.erase(std::remove(m_delegates.begin(), m_delegates.end(), delegate), m_delegates.end());
+        }
+
+        static DelegateType create(FunctionType&& function)
+        {
+            return DelegateType::create(std::move(function));
         }
 
     private:
-        delegate_t(FunctionType function) : m_function{function} {}
-
-        FunctionType m_function = nullptr;
+        std::vector<DelegateType> m_delegates;
     };
 }
 
 #define DEFINE_VOID_DELEGATE(DelegateTypeName, ...) using DelegateTypeName = ecs::delegate_t<void, ## __VA_ARGS__>
 
 #define DEFINE_DELEGATE_RET(DelegateTypeName, ReturnType, ...) using DelegateTypeName = ecs::delegate_t<ReturnType, ## __VA_ARGS__>
+
+#define DEFINE_MULTICAST_DELEGATE(DelegateTypeName, ...) using DelegateTypeName = ecs::mc_delegate_t< __VA_ARGS__>
--- ecs-test/src/test_delegates.cpp
+++ ecs-test/src/test_delegates.cpp
@@ -54,3 +54,55 @@ TEST_F(TestDelegates, TestDelegateWithReturnTypeAndArguments)
 
     ASSERT_EQ(delegate.invoke(5), 15);
 }
+
+TEST_F(TestDelegates, TestEmptyDelegate)
+{
+    DEFINE_VOID_DELEGATE(VoidDelegate);
+    VoidDelegate delegate;
+
+    ASSERT_THROW(delegate.invoke(), std::bad_function_call);
+}
+
+TEST_F(TestDelegates, TestMulticastDelegates)
+{
+    DEFINE_MULTICAST_DELEGATE(VoidMulticastDelegate);
+    VoidMulticastDelegate delegate;
+
+    delegate += VoidMulticastDelegate::create([&]() { m_counter += 1; });
+    delegate += VoidMulticastDelegate::create([&]() { m_counter += 1; });
+    delegate += VoidMulticastDelegate::create([&]() { m_counter += 1; });
+
+    delegate.broadcast();
+    ASSERT_EQ(m_counter, 3);
+}
+
+TEST_F(TestDelegates, TestMulticastDelegatesWithArguments)
+{
+    DEFINE_MULTICAST_DELEGATE(IntMulticastDelegate, int);
+    IntMulticastDelegate delegate;
+
+    delegate += IntMulticastDelegate::create([&](int i) { m_counter += i; });
+    delegate += IntMulticastDelegate::create([&](int i) { m_counter += i; });
+    delegate += IntMulticastDelegate::create([&](int i) { m_counter += i; });
+
+    delegate.broadcast(5);
+    ASSERT_EQ(m_counter, 15);
+}
+
+TEST_F(TestDelegates, TestMulticastDelegatesRemovingDelegate)
+{
+    DEFINE_MULTICAST_DELEGATE(MulticastDelegate);
+    MulticastDelegate delegate;
+
+    MulticastDelegate::DelegateType delegate1 = MulticastDelegate::create([&]() { m_counter += 1; });
+    MulticastDelegate::DelegateType delegate2 = MulticastDelegate::create([&]() { m_counter += 2; });
+    MulticastDelegate::DelegateType delegate3 = MulticastDelegate::create([&]() { m_counter += 3; });
+
+    delegate += delegate1;
+    delegate += delegate2;
+    delegate += delegate3;
+    delegate -= delegate2;
+
+    delegate.broadcast();
+    ASSERT_EQ(m_counter, 4);
+}
